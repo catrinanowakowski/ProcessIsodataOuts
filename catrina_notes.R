ac5af41b237dc13bd15c43a699c56b647f343625
catrinanowakowski <nowakowskicatherine@gmail.com>
2022-03-17 18:14:51 -0400

    adding data

--- catrina_notes.R
+++ catrina_notes.R
@@ -3,3 +3,12 @@
 
 pacman::p_load(sinew)
 sinew::makeOxyFile("R/functions/Lab_Std_plots_N.R")
+
+usethis::use_data_raw()
+
+
+#######################
+## Need to add a function for the test aa std runs 
+## Would be good to also have a function that does the AAstds alone 
+
+## eventualy add a trophic position function ect... 
